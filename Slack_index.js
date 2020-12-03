5ef03f39c94e6d670efc330fda6ca67672c0da1d
wapfel20 <12298553+wapfel20@users.noreply.github.com>
2020-12-03 12:01:07 -0600

    Update Slack_index.js
    updated to include distinction between exceeding forecasted budgets and actual budgets. Added graphics to the titles. Added ability to uncomment code for updates.

--- Slack_index.js
+++ Slack_index.js
@@ -4,60 +4,91 @@ const request = require(`request`);
 exports.notifySlack = async (pubsubEvent, context) => {
     const pubsubData = Buffer.from(pubsubEvent.data, 'base64').toString();
     const alert = JSON.parse(pubsubData);
-    
-    // Check to see if the budget has been exceeded. (without this, budget updates will be sent every 30 minutes)
-    if (alert.costAmount >= alert.budgetAmount) {
 
-    // Post message to the room
-    request({
-      uri: "<insert webhook url>",
-      method: "POST",
-      json: {
-	"blocks": [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*GCP Billing Alert* \n One of your GCP Projects has exceeded its monthly budget."
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Budget Exceeded:* " + alert.budgetDisplayName + "\n *Budget Start Date:* " + alert.costIntervalStart + "\n *Budget Amount:* " + alert.budgetAmount + "\n *Alert Threshold:* " + alert.alertThresholdExceeded + "\n *Current Spend:* " + alert.costAmount + "\n *Currency Type:* " + alert.currencyCode
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "OPEN THE GCP BILLING CONSOLE",
-						"emoji": true
-					},
-					"style": "primary",
-					"url": "https://console.cloud.google.com/billing"
-				}
-			]
-		}
-	]
-}               
-    }, function(error, response, body){
-    if(error) {
-        console.log(error);
+    // Format of message can be found here: https://cloud.google.com/billing/docs/how-to/budgets-programmatic-notifications#notification_format
+    let title = "", message = ""
+    // If there was an alert threshold breached
+    if ("alertThresholdExceeded" in alert) {
+        title = ":bangbang: GCP Budget Alert Threshold Exceeded :bangbang:"
+        message = `*Budget Name:* ${alert.budgetDisplayName}` +
+            `\n*Current Spend:* ${alert.costAmount}` +
+            `\n*Budget Amount Exceeded:* ${alert.budgetAmount}` +
+            `\n*Alert Threshold:* ${alert.alertThresholdExceeded * 100}%` +
+            `\n*Currency Type:* ${alert.currencyCode}` +
+            `\n*Budget Start Date:* ${alert.costIntervalStart}`
+    } else if ("forecastThresholdExceeded" in alert) {
+        title = ":warning: GCP Budget Forecast Threshold Exceeded :warning:"
+        message = `*Budget Name:* ${alert.budgetDisplayName}` +
+            `\n*Current Spend:* ${alert.costAmount}` +
+            `\n*Forecast Amount Exceeded:* ${alert.budgetAmount}` +
+            `\n*Forecast Threshold:* ${alert.forecastThresholdExceeded * 100}%` +
+            `\n*Currency Type:* ${alert.currencyCode}` +
+            `\n*Budget Start Date:* ${alert.costIntervalStart}`
     } else {
-        console.log(response.statusCode, body);
+        // Uncomment the section below if you wish to get general updates about a budget. This will be noisy! Updates are sent every 20 to 30 minutes per budget.
+        /*
+        title = "Budget Update"
+        message = `*Budget Name:* ${alert.budgetDisplayName}` +
+            `\n*Current Spend:* ${alert.costAmount}` +
+            `\n*Budget Amount:* ${alert.budgetAmount}` +
+            `\n*Currency Type:* ${alert.currencyCode}` +
+            `\n*Budget Start Date:* ${alert.costIntervalStart}`
+        */
     }
-});
 
-}
+    if (title != "" && message != "") {
+        // Post message to the room
+        request({
+            uri: process.env.SLACK_WEBHOOK,
+            method: "POST",
+            json: {
+                "blocks": [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": title,
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": message
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "View In GCP Billing Console",
+                                    "emoji": true
+                                },
+                                "style": "primary",
+                                "url": "https://console.cloud.google.com/billing"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(response.statusCode, body);
+            }
+        });
+    }
 };
+
+
